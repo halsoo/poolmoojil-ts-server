fa119db835d09df64796cf6950a6b84ebb7ca3f3
UKGU <UKGU@UKGUs-MacBookPro.local>
2020-06-07 22:39:40 +0900

    pg-ready final

---
+++ frontend/precache-manifest.a7301c38c98d84f04c1f2e4521462643.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73106777c1bd174b2a27750d0ee25d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9930984e67ace271552d",
+    "url": "/static/css/main.7f2db975.chunk.css"
+  },
+  {
+    "revision": "1c4be14c09cd5c8eac96",
+    "url": "/static/js/2.ab9ca8aa.chunk.js"
+  },
+  {
+    "revision": "348823c990b818d4b49c736f07b5685f",
+    "url": "/static/js/2.ab9ca8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9930984e67ace271552d",
+    "url": "/static/js/main.6da5d927.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "8ee54f7cb13ae5f24c1156babcb1a0d6",
+    "url": "/static/media/cart.8ee54f7c.png"
+  },
+  {
+    "revision": "69345d3f5f921968dcc66e1c7458fb57",
+    "url": "/static/media/gathering_img.69345d3f.png"
+  },
+  {
+    "revision": "f83c50f53f8fed57bae82da03c14ce0b",
+    "url": "/static/media/main_img.f83c50f5.png"
+  },
+  {
+    "revision": "d3f2255d32c699fbfedc60a9ced28bf7",
+    "url": "/static/media/main_logo.d3f2255d.png"
+  },
+  {
+    "revision": "50c1919b20fc34cc7ff81bf1d27c5072",
+    "url": "/static/media/package_img.50c1919b.jpg"
+  }
+]);
