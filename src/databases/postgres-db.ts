76d7e8a5970608be3c6c30cfa74cac539f1bb357
UKGU <UKGU@UKGUs-MacBookPro.local>
2020-06-08 02:44:13 +0900

    fix vercel.json error and add .vercelignore

--- src/databases/postgres-db.ts
+++ src/databases/postgres-db.ts
@@ -1,7 +1,19 @@
 import { createConnection } from 'typeorm';
+import { postgresTables } from './postgres-tables';
 
 export const postgresDB = async () => {
-    return await createConnection().then((connection) => {
+    return await createConnection({
+        type: 'postgres',
+        host: process.env.TYPEORM_HOST,
+        port: 5432,
+        username: process.env.TYPEORM_USERNAME,
+        password: process.env.TYPEORM_PASSWORD,
+        database: 'poolmoojil',
+        ssl: false,
+        entities: postgresTables,
+        logging: ['query', 'error'],
+        synchronize: false,
+    }).then((connection) => {
         console.log('Database connection established');
     });
 };
