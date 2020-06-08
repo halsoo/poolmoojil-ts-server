c3b42545532077ef996273ccdb9e284d2519a87e
UKGU <UKGU@UKGUs-MacBookPro.local>
2020-06-08 16:41:49 +0900

    intergrate deploy fixed api path

--- src/routes/basic-router.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-import { apiRouter } from './api-router';
-
-export const basicRouter = new Router();
-
-basicRouter.get('/', (ctx: any, next) => {
-    ctx.body = "it's alive!";
-    next();
-});
